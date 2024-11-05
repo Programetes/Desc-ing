ff6c427c0486dc7689ce38f15ee3c49339cd137f
Pedro Guimarães Bastos <pedroguibas123@gmail.com>
2024-11-05 00:32:40 -0300

    Jogos populares Comecei a sessão dos jogos populares, por enquanto, apenas com 4 jogos, mas poderemos adicionar mais posteriormente.

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,27 @@
-new Swiper('.cardList', {
-    
-    pagination: {
-      el: '.swiper-pagination',
+new Swiper('.cardWrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1
     },
-    
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    768: {
+      slidesPerView: 2
     },
-  });
+    1024: {
+      slidesPerView: 3
+    },
+  }
+});
