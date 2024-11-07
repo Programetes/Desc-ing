6a6d0b649c027c939322ac3597dd1ceb99282948
Pedro Guimarães Bastos <pedroguibas123@gmail.com>
2024-11-06 22:18:37 -0300

    Criação de conta e códigos para senha. Criei o endereço para a ciração de conta mas escolhi resolver algumas questões antes de continuar. escrevi um código para mostrar e esconder a senha.

--- javascript/login.js
+++ javascript/login.js
@@ -1,20 +1,49 @@
-var passwordShowing = false;
-
+var labelValueStore;
 
 function togglePassword() {
-    if(!passwordShowing) {
-      viewPasswordBtn.innerHTML = '<span class="material-symbols-outlined">visibility</span>';
-      passwordLogInput.type = 'text';
-      passwordShowing = true;
+  if(!this.parentElement.querySelector('.textInput').classList.contains('labeled'))
+    if(!this.classList.contains('showingPassword')) {
+      this.innerHTML = '<span class="material-symbols-outlined">visibility</span>';
+      this.parentElement.querySelector('.textInput').type = 'text';
+      this.classList.add('showingPassword');
     } else {
-      viewPasswordBtn.innerHTML = '<span class="material-symbols-outlined">visibility_off</span>';
-      passwordLogInput.type = 'password';
-      passwordShowing = false;
+      this.parentElement.querySelector('.textInput').type = 'password';
+      this.innerHTML = '<span class="material-symbols-outlined">visibility_off</span>';
+      this.classList.remove('showingPassword');
     }
 }
 
-  
-const viewPasswordBtn = document.getElementById('viewPassword');
-viewPasswordBtn.addEventListener('click', togglePassword);
+function removeInputLabel() {
+  if(this.classList.contains('labeled')) {
+    labelValueStore = this.value;
+    this.value = '';
+    this.style = '';
+    this.classList.remove('labeled');
 
-const passwordLogInput = document.getElementById('passwordLogInput');
+    if(this.classList.contains('password')) {
+      this.type = 'password';
+    }
+  }
+}
+
+function focusCheck() {
+  if(this.value == ''){
+    this.value = labelValueStore;
+    this.classList.add('labeled');
+    this.style = 'color: #ffffff59';
+    this.type = 'text';
+    this.parentElement.parentElement.querySelector('.viewPasswordBtn').innerHTML = '<span class="material-symbols-outlined">visibility_off</span>';
+  }
+}
+
+const viewPasswordBtn = document.querySelectorAll('.viewPasswordBtn');
+viewPasswordBtn.forEach(function(elem) {
+  elem.addEventListener('click', togglePassword);
+});
+
+
+const passwordLogInput = document.querySelectorAll('.textInput');
+passwordLogInput.forEach(function(elem) {
+  elem.addEventListener('focus', removeInputLabel);
+  elem.addEventListener('focusout', focusCheck);
+});
