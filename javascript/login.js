c9796e9ca81af41c2e720a17baa42defddb14e2c
Pedroguibas <pedroguibas123@gmail.com>
2024-11-07 11:15:15 -0300

    Mudanças visuais e de código Ainda estou tentando entender como deixar respnosivo, retirei um código desnecessário em js.

--- javascript/login.js
+++ javascript/login.js
@@ -1,38 +1,14 @@
 var labelValueStore;
 
 function togglePassword() {
-  if(!this.parentElement.querySelector('.textInput').classList.contains('labeled'))
-    if(!this.classList.contains('showingPassword')) {
-      this.innerHTML = '<span class="material-symbols-outlined">visibility</span>';
-      this.parentElement.querySelector('.textInput').type = 'text';
-      this.classList.add('showingPassword');
-    } else {
-      this.parentElement.querySelector('.textInput').type = 'password';
-      this.innerHTML = '<span class="material-symbols-outlined">visibility_off</span>';
-      this.classList.remove('showingPassword');
-    }
-}
-
-function removeInputLabel() {
-  if(this.classList.contains('labeled')) {
-    labelValueStore = this.value;
-    this.value = '';
-    this.style = '';
-    this.classList.remove('labeled');
-
-    if(this.classList.contains('password')) {
-      this.type = 'password';
-    }
-  }
-}
-
-function focusCheck() {
-  if(this.value == ''){
-    this.value = labelValueStore;
-    this.classList.add('labeled');
-    this.style = 'color: #ffffff59';
-    this.type = 'text';
-    this.parentElement.parentElement.querySelector('.viewPasswordBtn').innerHTML = '<span class="material-symbols-outlined">visibility_off</span>';
+  if(!this.classList.contains('showingPassword')) {
+    this.innerHTML = '<span class="material-symbols-outlined">visibility</span>';
+    this.parentElement.querySelector('.textInput').type = 'text';
+    this.classList.add('showingPassword');
+  } else {
+    this.parentElement.querySelector('.textInput').type = 'password';
+    this.innerHTML = '<span class="material-symbols-outlined">visibility_off</span>';
+    this.classList.remove('showingPassword');
   }
 }
 
@@ -43,7 +19,3 @@ viewPasswordBtn.forEach(function(elem) {
 
 
 const passwordLogInput = document.querySelectorAll('.textInput');
-passwordLogInput.forEach(function(elem) {
-  elem.addEventListener('focus', removeInputLabel);
-  elem.addEventListener('focusout', focusCheck);
-});
