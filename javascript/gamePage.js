ab2f1cef3ba8e4bb44f3b46767da12a1905790bc
Pedroguibas <pedroguibas123@gmail.com>
2024-11-09 09:40:32 -0300

    Estilo página de jogos Desenvolvi uma configuração de estilo para a página do god of war como uma forma de teste.

--- javascript/gamePage.js
+++ javascript/gamePage.js
@@ -2,11 +2,11 @@
 function dropdown() {
     if (!this.querySelector('.buttonIcon').classList.contains('rotated')) {
         this.querySelector('.buttonIcon').classList.add('rotated');
-        this.parentElement.querySelector('.gameDropContent').classList.remove('gameDropHidden');
+        this.parentElement.parentElement.querySelector('.gameDropContent').classList.remove('gameDropHidden');
     }
     else {
         this.querySelector('.buttonIcon').classList.remove('rotated');
-        this.parentElement.querySelector('.gameDropContent').classList.add('gameDropHidden');
+        this.parentElement.parentElement.querySelector('.gameDropContent').classList.add('gameDropHidden');
     }
 }
 
